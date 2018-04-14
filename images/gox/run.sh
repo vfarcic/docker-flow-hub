7b234aea804257a3ecaffd4315d3597220cb8866
Viktor <viktor@farcic.com>
2018-04-14 18:07:16 +0200

    Reduced the number of binaries

--- images/gox/run.sh
+++ images/gox/run.sh
@@ -2,4 +2,4 @@
 
 go get -d -v -t
 
-gox --output="$1_{{.OS}}_{{.Arch}}" -osarch="linux/386 linux/amd64 linux/arm windows/386 windows/amd64 darwin/386 darwin/amd64" -arch="386 amd64 arm"
+gox --output="$1_{{.OS}}_{{.Arch}}" -osarch="linux/amd64 linux/arm windows/amd64 darwin/amd64"
